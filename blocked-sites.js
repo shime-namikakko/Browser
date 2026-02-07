6bbe1272a2a67481843f840e95848e00eb9d4188
shime-namikakko <shime.sonocc@gmail.com>
2026-02-07 20:59:52 +0900

    Update blocked-sites.js

--- blocked-sites.js
+++ blocked-sites.js
@@ -1,45 +1,42 @@
 // 開けない主要なサイト一覧
-// このファイルを編集することで、リストを簡単に更新できます
 
 const BLOCKED_SITES = [
     {
         name: 'Google系サービス',
         sites: [
-            'Gmail (mail.google.com)',
-            'Google検索 (google.com)',
-            'YouTube (youtube.com)',
-            'Google Drive (drive.google.com)',
-            'Google Maps (maps.google.com)',
-            'Google Calendar (calendar.google.com)'
+            'Gmail',
+            'Google検索',
+            'YouTube',
+            'Google Drive',
+            'Google Maps',
+            'Google Calendar'
         ]
     },
     {
         name: 'SNS',
         sites: [
-            'Twitter/X (twitter.com, x.com)',
-            'Facebook (facebook.com)',
-            'Instagram (instagram.com)',
-            'LinkedIn (linkedin.com)',
-            'TikTok (tiktok.com)'
+            'Twitter/X',
+            'Facebook',
+            'Instagram',
+            'LinkedIn',
+            'TikTok',
+            'ニコニコ動画'
         ]
     },
     {
         name: 'その他の主要サービス',
         sites: [
-            'GitHub (github.com)',
-            'Netflix (netflix.com)',
-            'Amazon (amazon.com)',
-            'PayPal (paypal.com)',
-            'Apple ID (appleid.apple.com)'
+            'Netflix',
+            'Amazon',
+            'Apple ID'
         ]
     },
     {
         name: '使えるサイトの例',
         sites: [
-            'Wikipedia (wikipedia.org)',
-            'Archive.org (archive.org)',
-            'Example.com (example.com)',
-            'MDN Web Docs (developer.mozilla.org)'
+            'Wikipedia',
+            'bilibili動画',
+            'その他の大体のサイト'
         ]
     }
 ];
